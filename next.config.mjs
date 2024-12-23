80a2aec39337ba2d60a0b86f627a812ae72b8f18
Samer Timani <pandatech1999@gmail.com>
2024-12-23 18:00:31 +0300

    routing fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true, // Enforces React best practices
+    swcMinify: true,       // Enables faster builds
+    experimental: {
+        appDir: true,        // Ensures the app directory routing is enabled
+    },
+};
 
 export default nextConfig;
