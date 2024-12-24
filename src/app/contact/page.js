8f9d89f0df923ba5349bd9230691f9aa11f37dc4
Samer Timani <pandatech1999@gmail.com>
2024-12-24 11:28:09 +0300

    Fixes

---
+++ src/app/contact/page.js
@@ -0,0 +1,13 @@
+
+import ContactUs from "../components/Contact_US/Contact_us";
+import Layout from "../components/Layout";
+
+export default function AboutPage() {
+    return (
+        <div className="transition-colors">
+            <Layout>
+                <ContactUs />
+            </Layout>
+        </div>
+    );
+}
