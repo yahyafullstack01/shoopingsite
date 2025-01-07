94057bfacf54bffb9dbe9a632acc93049802eaee
yahyafullstack01 <yahyafullstack01@gmail.com>
2025-01-07 19:50:38 +0200

    add text and language to conditions

--- src/app/components/Conditions/Conditions.jsx
+++ src/app/components/Conditions/Conditions.jsx
@@ -1,84 +1,83 @@
+"use client";
 import React from "react";
-
+import { useLanguage } from "../../Functions/useLanguage";
 const Conditions = () => {
+    const { translateList, language, setLanguage } = useLanguage(); // Access the language context
+    const menuItems = translateList("Conditions", "hero");
   return (
     <div className="bg-gray-50 text-gray-800 p-8 md:p-16">
       <h1 className="text-2xl md:text-4xl font-bold text-center mb-8">
-        Повернення та обмін товару
+        {menuItems[0]}
       </h1>
 
       <section className="mb-8">
         <h2 className="text-xl md:text-2xl font-semibold mb-4 ">
-          ЗАГАЛЬНІ УМОВИ ПОВЕРНЕННЯ ТА ОБМІНУ ТОВАРУ:
+        {menuItems[1]}
         </h2>
         <ul className="list-disc list-inside space-y-2">
-          <li>Повернення та обмін товарів можливе протягом 14 днів після отримання.</li>
-          <li>Обмін можливий як на інший розмір та колір, так і на будь-яку іншу річ.</li>
+          <li>{menuItems[2]}</li>
+          <li>{menuItems[3]}</li>
           <li>
-            У разі отримання товару неналежної якості обмін та повернення оплачується нашим магазином. 
-            Якщо вам доставили не той товар, який ви замовляли, він теж відноситься до товару неналежної 
-            якості та доставки оплачуються нами.
+          {menuItems[3]}
           </li>
           <li>
-            Товари належної якості підлягають як поверненню, і обміну без пояснення причин. У разі 
-            повернення/обміну товарів належної якості (без браку), поштові витрати оплачує покупець.
+          {menuItems[4]}
           </li>
         </ul>
       </section>
 
       <section className="mb-8">
         <h2 className="text-xl md:text-2xl font-semibold mb-4">
-          Ви можете повернути або обміняти товар належної якості, якщо:
+        {menuItems[5]}
         </h2>
         <ul className="list-disc list-inside space-y-2">
-          <li>Товар не був у вживанні і не має слідів використання: подряпин, сколів, потертостей, плям тощо;</li>
-          <li>Товар зберігає товарний вигляд та свої споживчі властивості.</li>
+          <li>{menuItems[6]}</li>
+          <li>{menuItems[7]}</li>
         </ul>
       </section>
 
       <section className="mb-8">
         <h2 className="text-xl md:text-2xl font-semibold mb-4">
-          ОБМІНУ ТА ПОВЕРНЕННЮ НЕ ПІДЛЯГАЮТЬ:
+        {menuItems[8]}
         </h2>
         <ul className="list-disc list-inside space-y-2">
-          <li>Вироби, що пошиті за мірками клієнта;</li>
-          <li>Вироби, у яких змінена довжина виробу/ фасон за проханням замовника;</li>
+          <li>{menuItems[9]}</li>
+          <li>{menuItems[10]}</li>
         </ul>
         <p className="mt-2">
-          У даному випадку можлива лише переробка за попередньою домовленістю.
+        {menuItems[11]}
         </p>
       </section>
 
       <section className="mb-8">
         <h2 className="text-xl md:text-2xl font-semibold mb-4">
-          Способи доставки
+        {menuItems[12]}
         </h2>
         <ul className="list-disc list-inside space-y-2">
           <li>
-            <span className="font-semibold">Нова пошта:</span> доставка у відділення та поштомати
+            <span className="font-semibold">{menuItems[13]}</span> {menuItems[14]}
           </li>
           <li>
-            <span className="font-semibold">Укр пошта:</span> доставка у відділення
+            <span className="font-semibold">{menuItems[15]}</span> {menuItems[16]}
           </li>
           <li>
-            <span className="font-semibold">Закордон за домовленістю:</span> Нова пошта або Укр пошта
+            <span className="font-semibold">{menuItems[17]}</span> {menuItems[18]}
           </li>
           <li>
-            <span className="font-semibold">Самовивіз:</span> з міста Харків або Івано-Франківськ
+            <span className="font-semibold">{menuItems[19]}</span> {menuItems[20]}
           </li>
         </ul>
       </section>
 
       <section>
         <h2 className="text-xl md:text-2xl font-semibold mb-4">
-          Додаткова інформація:
+          Д{menuItems[21]}
         </h2>
         <p className="mb-2">
-          Ми зможемо оформити повернення тоді, коли отримаємо Вашу посилку на нашому складі. Це пересилання може зайняти 
-          до 5-7 робочих днів без урахування вихідних та святкових днів.
+        {menuItems[22]}
         </p>
         <p>
-          Для обміну або повернення телефонуйте у технічну підтримку (24/7):{" "}
+        {menuItems[23]}{" "}
           <span className="font-bold text-blue-600">+38-067-579-5497</span>
         </p>
       </section>
