6308e5d703208dce7bc734e104f68109e8526251
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2025-01-17 15:39:27 +0100

    location-

--- pages/api/geolocation.js
+++ pages/api/geolocation.js
@@ -1,9 +1,14 @@
 export default async function handler(req, res) {
     try {
-        const response = await fetch(`https://ipinfo.io/json?token=ff259f48889575`);
+        // Отримуємо IP клієнта
+        const clientIp = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+
+        // Запит до ipinfo.io з реальним IP
+        const response = await fetch(`https://ipinfo.io/${clientIp}/json?token=ff259f48889575`);
         if (!response.ok) {
             throw new Error("Failed to fetch geolocation data");
         }
+
         const data = await response.json();
         res.status(200).json(data);
     } catch (error) {
@@ -11,3 +16,4 @@ export default async function handler(req, res) {
         res.status(500).json({ error: "Failed to fetch geolocation data" });
     }
 }
+
