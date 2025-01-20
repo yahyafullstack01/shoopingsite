76b13eec7694a3a9dae49fd29b8407e0f2faffba
yahyafullstack01 <yahyafullstack01@gmail.com>
2025-01-20 15:05:41 +0200

    sizeTable

--- src/app/components/SizeChart/SizeChart.js
+++ src/app/components/SizeChart/SizeChart.js
@@ -1,19 +1,23 @@
 import React from "react";
-
+import { useLanguage } from "../../Functions/useLanguage"; 
 export default function SizeChart({ onClose }) {
+
+  const {  translateList } = useLanguage(); 
+  const menuItems = translateList("SizeTable", "header");
+  
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50">
       <div className="bg-white dark:bg-black rounded-lg shadow-lg p-4 md:p-6 w-11/12 sm:w-10/12 md:w-2/3 lg:w-1/2">
-        <h2 className="text-lg font-bold mb-4 text-center">Розмірна сітка</h2>
+        <h2 className="text-lg font-bold mb-4 text-center">{menuItems[0]}</h2>
         <div className="overflow-auto">
           <table className="w-full border-collapse border border-gray-300 dark:border-gray-900 text-xs sm:text-sm">
             <thead>
               <tr className="bg-gray-200 dark:bg-gray-900">
-                <th className="border border-gray-300 px-2 sm:px-4 py-2">Розмір (UA)</th>
-                <th className="border border-gray-300 px-2 sm:px-4 py-2">Розмір (EU)</th>
-                <th className="border border-gray-300 px-2 sm:px-4 py-2">Груди (см)</th>
-                <th className="border border-gray-300 px-2 sm:px-4 py-2">Талія (см)</th>
-                <th className="border border-gray-300 px-2 sm:px-4 py-2">Стегна (см)</th>
+                <th className="border border-gray-300 px-2 sm:px-4 py-2">{menuItems[1]}</th>
+                <th className="border border-gray-300 px-2 sm:px-4 py-2">{menuItems[2]}</th>
+                <th className="border border-gray-300 px-2 sm:px-4 py-2">{menuItems[3]}</th>
+                <th className="border border-gray-300 px-2 sm:px-4 py-2">{menuItems[4]}</th>
+                <th className="border border-gray-300 px-2 sm:px-4 py-2">{menuItems[5]}</th>
               </tr>
             </thead>
             <tbody>
@@ -59,7 +63,7 @@ export default function SizeChart({ onClose }) {
           onClick={onClose}
           className="mt-6 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 block mx-auto"
         >
-          Закрити
+          {menuItems[6]}
         </button>
       </div>
     </div>
