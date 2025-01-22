997ab79f30717b6344fc28526d60026585edb3ec
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2025-01-22 09:25:11 +0100

    Update Header.jsx

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -23,14 +23,17 @@ const Header = React.memo(({ isDarkMode, toggleDarkMode }) => {
       <div className="flex-shrink-0 ml-0 sm:ml-8">
         <Link href="/" aria-label="Home">
         <h1 className="sr-only">Latore Atelier</h1>
-          <Image
-            src="/light-logo.avif"
-            alt="Latore Atelier Logo – High-Quality Clothing"
-            width={180}
-            height={160}
-            quality={100}
-            className={`${isDarkMode ? "filter invert" : ""} w-[80px] h-[60px] sm:w-[80px] sm:h-[60px] md:w-[80px] md:h-[60px] lg:w-[120px] lg:h-[80px]`}
-          />
+        <Image
+  src="/light-logo.avif"
+  alt="Latore Atelier Logo – High-Quality Clothing"
+  width={120} 
+  height={80} 
+  quality={100}
+  sizes="(max-width: 768px) 80px, (max-width: 1200px) 120px"
+  className={`${isDarkMode ? "filter invert" : ""} w-[80px] h-[60px] sm:w-[80px] sm:h-[60px] md:w-[80px] md:h-[60px] lg:w-[120px] lg:h-[80px]`}
+  priority 
+/>
+
         </Link>
       </div>
       <nav aria-label="Main Navigation" className="flex items-center space-x-4 lg:space-x-6">
