7151243408b841bb1ff187da93646fe838800396
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2025-01-23 23:37:52 +0100

    hero

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -34,7 +34,7 @@ const Header = React.memo(({ isDarkMode, toggleDarkMode }) => {
     layout="intrinsic"
     width={128} // Зберігає співвідношення ширини
     height={69} // Зберігає співвідношення висоти
-    quality={90}
+    quality={100}
     sizes="(max-width: 480px) 50px, (max-width: 768px) 80px, (max-width: 1200px) 100px, 128px"
     className={`w-16 h-auto sm:w-24 md:w-32 lg:w-40 ${
     isDarkMode ? "filter invert" : ""
--- src/app/components/Hero/Hero.jsx
+++ src/app/components/Hero/Hero.jsx
@@ -17,11 +17,11 @@ export default function Hero() {
             <Image
               src="/1.jpg"
               alt="Left showcase of Atelier fashion"
-              width={200} // Adjusted width
-              height={250} // Adjusted height
+              width={400} // Adjusted width
+              height={600} // Adjusted height
               priority
               sizes="(max-width: 640px) 100vw, (max-width: 768px) 90vw, (max-width: 1200px) 50vw, 400px"
-             className="rounded-none object-cover shadow-2xl shadow-gray-800 dark:shadow-gray-400 transition-transform duration-300 ease-in-out focus:scale-110 hover:scale-110 group-hover:scale-110 cursor-pointer w-32 sm:w-[200px] md:w-[300px] lg:w-[300px]"
+             className="rounded-none object-cover shadow-2xl shadow-gray-800 dark:shadow-gray-400 transition-transform duration-300 ease-in-out focus:scale-110 hover:scale-110 group-hover:scale-110 cursor-pointer w-36 sm:w-[250px] md:w-[300px] lg:w-[400px]"
             />
           </div>
 
@@ -29,14 +29,14 @@ export default function Hero() {
             <h1 className="text-2xl sm:text-4xl md:text-6xl xl:text-7xl font-bold tracking-wide whitespace-nowrap text-center inline-block relative text-gray-900 dark:text-gray-100">
               LATORE A
               <span className="relative z-10">TELIER</span>
-              <span className="absolute top-[-60px] right-[-90px] sm:top-[-220px] sm:right-[-260px] md:top-[-240px] md:right-[-260px] xl:top-[-340px] xl:right-[-340px] z-0 mobile-320">
+              <span className="absolute top-[-90px] right-[-90px] sm:top-[-220px] sm:right-[-260px] md:top-[-240px] md:right-[-220px] xl:top-[-340px] xl:right-[-280px] z-0 mobile-320">
                 <Image
                   src="/10.jpg"
                   alt="Showcase of Atelier design – Right"
-                  width={300} // Adjusted width
-                  height={300} // Adjusted height
-                  sizes="(max-width: 768px) 90vw, (max-width: 1200px) 45vw, 300px"
-                  className="w-32 h-32 sm:w-[250px] sm:h-[250px] md:w-[300px] md:h-[300px] lg:w-[300px] lg:h-[300px] xl:w-[300px] xl:h-[300px] object-cover shadow-2xl shadow-gray-800 dark:shadow-gray-400 transition-transform duration-300 ease-in-out focus:scale-110 hover:scale-110 group-hover:scale-110"
+                  width={400} // Adjusted width
+                  height={400} // Adjusted height
+                  sizes="(max-width: 768px) 90vw, (max-width: 1200px) 45vw, 400px"
+                  className="w-36 h-36 sm:w-[250px] sm:h-[250px] md:w-[300px] md:h-[300px] lg:w-[400px] lg:h-[400px] xl:w-[500px] xl:h-[500px] object-cover shadow-2xl shadow-gray-800 dark:shadow-gray-400 transition-transform duration-300 ease-in-out focus:scale-110 hover:scale-110 group-hover:scale-110"
                 />
               </span>
             </h1>
@@ -48,10 +48,10 @@ export default function Hero() {
             <Image
               src="/5.jpg"
               alt="Showcase collection – Bottom left image"
-              width={300} // Adjusted width
-              height={450} // Adjusted height
-              sizes="(max-width: 768px) 90vw, (max-width: 1200px) 45vw, 300px"
-              className="rounded-none object-cover shadow-2xl shadow-gray-800 dark:shadow-gray-400 transition-transform duration-300 ease-in-out focus:scale-110 hover:scale-110 group-hover:scale-110 w-32 sm:w-[200px] md:w-[300px] lg:w-[300px] ml-8 sm:ml-36 h-auto"
+              width={400} // Adjusted width
+              height={600} // Adjusted height
+              sizes="(max-width: 768px) 90vw, (max-width: 1200px) 45vw, 400px"
+              className="rounded-none object-cover shadow-2xl shadow-gray-800 dark:shadow-gray-400 transition-transform duration-300 ease-in-out focus:scale-110 hover:scale-110 group-hover:scale-110 w-36 sm:w-[250px] md:w-[300px] lg:w-[400px] ml-8 sm:ml-36 h-auto"
             />
           </div>
 
