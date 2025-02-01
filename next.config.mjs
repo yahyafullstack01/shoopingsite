3722448253cfec15161ce830c727b062d3b7e8d5
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2025-02-01 11:30:55 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true, 
-   
-     
-};
-{/*
-const nextConfig = {
-    reactStrictMode: true, // Enforces React best practices
-    swcMinify: true,       // Enables faster builds
-    experimental: {
-        appDir: true,        // Ensures the app directory routing is enabled
+    reactStrictMode: true, // Увімкнення строгого режиму React
+    images: {
+      formats: ['image/webp'], // Підтримка формату WebP
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'shoopingsite-my9e.vercel.app', // Ваш основний домен
+        },
+        // Додайте сюди інші домени, якщо потрібно
+        // {
+        //   protocol: 'https',
+        //   hostname: 'cdn.example.com',
+        // },
+        // {
+        //   protocol: 'https',
+        //   hostname: 'images.unsplash.com',
+        // },
+      ],
     },
-};
-*/}
-export default nextConfig;
+  };
+  
+  export default nextConfig;
