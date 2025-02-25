be3b48afec8df058a635c51bc62706bf04950599
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2025-02-25 11:12:25 +0100

    Update sendEmail.js

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -43,10 +43,7 @@ export default async function handler(req, res) {
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
-    },
-    tls: {
-      rejectUnauthorized: false,
-    },
+    }
   });
 
   try {
