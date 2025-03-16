0f52215bdeedbce01d26d32118293f997dbe9658
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2025-03-16 22:07:45 +0100

    ico

--- src/app/layout.js
+++ src/app/layout.js
@@ -35,6 +35,9 @@ export default function RootLayout({ children }) {
                 <meta property="og:url" content="https://shoopingsite-my9e.vercel.app" />
                 <meta property="og:type" content="website" />
                 <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+
                 <link rel="canonical" href="https://shoopingsite-my9e.vercel.app" />
                 <link rel="preload" href="/fonts/Montserrat-Thin.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
 <link rel="preload" href="/fonts/Montserrat-ThinItalic.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
