5ceb544500e4028c7ff40addcd96b36eed796682
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2025-03-28 16:27:26 +0100

    Update layout.tsx

--- src/app/contact/layout.tsx
+++ src/app/contact/layout.tsx
@@ -9,8 +9,9 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <main className="min-h-screen transition-colors">
+      {children}
+    </main>
+    
   )
 }
