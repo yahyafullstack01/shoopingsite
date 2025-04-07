0172ea7e39f1efc3489b468b69973b0e5cd34cfe
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2025-04-07 11:10:11 +0200

    Update Hero.jsx

--- src/app/components/Hero/Hero.jsx
+++ src/app/components/Hero/Hero.jsx
@@ -57,7 +57,6 @@ const Hero = () => {
               muted
               playsInline
               preload="metadata"
-              poster="/hoom/hero2.avif"
               className="w-full h-auto max-h-[400px] object-cover rounded-lg"
             />
           </div>
