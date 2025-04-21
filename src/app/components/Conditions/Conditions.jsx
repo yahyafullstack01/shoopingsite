ef6b00f88dcf65338b55096cc66065f372df3a16
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2025-04-21 14:02:10 +0200

    condition

--- src/app/components/Conditions/Conditions.jsx
+++ src/app/components/Conditions/Conditions.jsx
@@ -50,6 +50,35 @@ const Conditions = () => {
       </h2>
       <p>{menuItems[14]}</p>
       <p>{menuItems[15]} <a href="tel:+380675795497" style={{ fontWeight: "bold", color: "#000", textDecoration: "underline" }}>+380 97 367 82 57</a></p>
+      <h2 style={{ fontWeight: "bold", fontSize: "20px", marginTop: "24px", marginBottom: "12px" }}>
+        {menuItems[16]}
+      </h2>
+      <ul style={{ paddingLeft: "20px", listStyleType: "disc" }}>
+        <li>{menuItems[17]}</li>
+        <li>{menuItems[18]}</li>
+        <li>{menuItems[19]}</li>
+        <li>{menuItems[20]}</li>
+        <li>{menuItems[21]}</li>
+        <li>{menuItems[22]}</li>
+        <li>{menuItems[23]}</li>
+        <li>{menuItems[24]}</li>
+        <li>{menuItems[25]}</li>
+        <li>{menuItems[26]}</li>
+        <li>{menuItems[27]}</li>
+        <li>{menuItems[28]}</li>
+        <li>{menuItems[29]}</li>
+        <li>{menuItems[30]}</li>
+        <li>{menuItems[31]}</li>
+        <li>{menuItems[32]}</li>
+        <li>{menuItems[33]}</li>
+        <li>{menuItems[34]}</li>
+        <li>{menuItems[35]}</li>
+        <li>{menuItems[36]}</li>
+        <li>{menuItems[37]}</li>
+        <li>{menuItems[38]}</li>
+        <li>{menuItems[39]}</li>
+        <li>{menuItems[40]}</li>
+      </ul>
     </main>
   );
 };
