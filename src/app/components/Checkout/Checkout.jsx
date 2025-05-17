1b9d0734295a50bbfb7c46a404c50fd0e4c09f9a
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2025-05-17 13:31:41 +0200

    Update Checkout.jsx

--- src/app/components/Checkout/Checkout.jsx
+++ src/app/components/Checkout/Checkout.jsx
@@ -184,7 +184,9 @@ const handleFondyPayment = async (order) => {
         amount: order.total,
         resultUrl: `${window.location.origin}/success`,
         serverUrl: `${BACKEND_URL}/api/payments/fondy-callback`,
-        order: savedOrder, // 🟡 ПЕРЕДАЄМО повністю order, не orderId!
+        orderId: savedOrder._id,
+  currency: 'UAH', // обов’язково вказати!
+  product: 'Оплата замовлення на Latoré'
       }),
     });
 
