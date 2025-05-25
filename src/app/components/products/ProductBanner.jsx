2bc610febc8bf721aa4215a253ff7d61f3326981
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2025-05-25 21:58:59 +0200

    Update ProductBanner.jsx

--- src/app/components/products/ProductBanner.jsx
+++ src/app/components/products/ProductBanner.jsx
@@ -38,7 +38,10 @@ const [selectedSize, setSelectedSize] = useState('');
   }, [selectedProduct]);
   const handleAddToCart = async ({ product, selectedColor, selectedSize, quantity }) => {
     const sessionId = getSessionId();
-    const name = product.translations?.[language]?.name || product.name || product.title;
+    const name = (product.translations?.[language]?.name || product.name || product.title)
+    .replace(/грн|₴/gi, '')   // ← очищення
+    .trim();
+
     const price = product.price;
     if (!sessionId) {
       alert("Не вдалося створити сесію. Спробуйте оновити сторінку.");
