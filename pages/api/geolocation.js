c505f0d99d9c12f4bbd87bb30f51ec7b45ad7c62
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2025-05-28 12:06:14 +0200

    Update geolocation.js

--- pages/api/geolocation.js
+++ pages/api/geolocation.js
@@ -1,4 +1,4 @@
-{/*export default async function handler(req, res) {
+export default async function handler(req, res) {
     try {
         // Отримуємо IP клієнта
         const clientIp = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
@@ -16,4 +16,4 @@
         res.status(500).json({ error: "Failed to fetch geolocation data" });
     }
 }
-*/}
+
