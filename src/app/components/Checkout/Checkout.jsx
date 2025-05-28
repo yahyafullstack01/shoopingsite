7b4873532bee82d686a8ccde44bd64a638142ec7
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2025-05-28 22:24:04 +0200

    Update Checkout.jsx

--- src/app/components/Checkout/Checkout.jsx
+++ src/app/components/Checkout/Checkout.jsx
@@ -606,7 +606,7 @@ const handleWayforpayPayment = async (order) => {
           className="w-full p-2 border rounded"
         />
   
-        {/* Оплата8
+        {/* Оплата8 */}
         <div>
           <label className="block mb-1 font-medium">Оплата</label>
           <div className="space-y-2">
@@ -660,7 +660,7 @@ const handleWayforpayPayment = async (order) => {
                   onChange={() => setOnlinePaymentMethod('stripe')}
                 />
                 <span>Stripe (🌍 USD / EUR)</span>
-              </label> 
+              </label> */}
               <label className="flex items-center space-x-2">
   <input
     type="radio"
@@ -674,11 +674,11 @@ const handleWayforpayPayment = async (order) => {
             </div> 
           </div>
         )}
-*/}
+
  
 
       
-        {/* Кнопка 
+        {/* Кнопка */}
         <button
           type="submit"
           className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
@@ -686,14 +686,14 @@ const handleWayforpayPayment = async (order) => {
         >
           Оплатити замовлення
         </button> 
-        {/* Кнопка замовити без оплати 
+        {/* Кнопка замовити без оплати */}
 <button
   type="button"
   onClick={handleOrderWithoutPayment}
   className="mt-2 ml-4 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
 >
   Замовити без оплати
-</button>*/}
+</button>
       </form>
     </div>
   );
