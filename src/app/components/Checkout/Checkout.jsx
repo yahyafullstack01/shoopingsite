156a27758369e45b278e8b30ec0cfde4af63892c
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2025-05-29 10:13:57 +0200

    Update Checkout.jsx

--- src/app/components/Checkout/Checkout.jsx
+++ src/app/components/Checkout/Checkout.jsx
@@ -272,7 +272,8 @@ const handleWayforpayPayment = async (order) => {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        resultUrl: `${window.location.origin}/payment-success`,
+        resultUrl: `${process.env.NEXT_PUBLIC_BASE_URL}/api/payments/wayforpay/success`,
+
  serverUrl: `${BACKEND_URL}/api/payments/wayforpay/callback`, // ✅
         order, // ✅ все замовлення, включно з sessionId
       }),
