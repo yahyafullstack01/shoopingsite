73a4efc0fc1a22d754f5d78214271a89b2263448
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2025-06-29 21:13:54 +0200

    m

--- src/app/payment-success/page.js
+++ src/app/payment-success/page.js
@@ -1,5 +1,10 @@
+import { Suspense } from 'react';
 import PaymentSuccessPage from '../components/PaymentSuccessPage/PaymentSuccessPage.jsx';
 
 export default function Page() {
-  return <PaymentSuccessPage />;
+  return (
+    <Suspense fallback={<div className="text-center p-4">Завантаження...</div>}>
+      <PaymentSuccessPage />
+    </Suspense>
+  );
 }
