a447d73a8554c01f7ec4240624aee95e0766b763
yahyafullstack01 <yahyafullstack01@gmail.com>
2025-09-29 22:41:00 +0200

    add

--- src/app/data/products.js
+++ src/app/data/products.js
@@ -630,7 +630,7 @@ const products = [
   isNew: true,
   sku: "98820",
   size: "XS S M L XL",
-  category: "Costumes",
+  category: "costumes",
   image: "/Costumes/Olive Suit/1.avif",
   images: [
     "/Costumes/Olive Suit/1.avif",
@@ -642,7 +642,7 @@ const products = [
   sizes: ["XS", "S", "M", "L", "XL"],
   translations: {
     EN: {
-      category: "Costumes",
+      category: "costumes",
       name: "Olive Suit",
       description: [
         "This suit combines classic elegance with a fresh color. The clean cut and noble shade make it a versatile choice for both business meetings and stylish urban looks.",
