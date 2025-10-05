f0c99baf30f259b872bd352ab52a8310579a4070
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2025-10-05 13:26:39 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,16 +4,13 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
-      // бекенд на Render
       { protocol: 'https', hostname: 'shoopingsite-backend-1.onrender.com', pathname: '/uploads/**' },
-
-      // якщо є Cloudinary/CDN — додай і його
-      // { protocol: 'https', hostname: 'res.cloudinary.com', pathname: '/**' },
-
-      // якщо якісь картинки все ж із фронт-домену
       { protocol: 'https', hostname: 'latore.store' },
       { protocol: 'https', hostname: 'www.latore.store' },
     ],
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [320,480,640,768,1024,1280,1536,1920,2560],
   },
 };
+
 export default nextConfig;
