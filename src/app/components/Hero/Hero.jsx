96450d99c86376fab69fa1c1c354beaccb2a0925
yahyafullstack01 <yahyafullstack01@gmail.com>
2026-02-06 10:54:10 +0100

    change mobile version images banner

--- src/app/components/Hero/Hero.jsx
+++ src/app/components/Hero/Hero.jsx
@@ -64,7 +64,7 @@ export default function Hero() {
 {
   type: "image",
   src: "/hoom/hero1.PNG",
-  mobileSrc: "/hoom/heromobile1.PNG",
+  mobileSrc: "/hoom/1.PNG",
   alt: "Latore banner 1",
 
   ctaText: t[4],
@@ -95,7 +95,7 @@ export default function Hero() {
       {
   type: "image",
   src: "/hoom/hero2.PNG",
-  mobileSrc: "/hoom/heromobile2.PNG",
+  mobileSrc: "/hoom/2.PNG",
   alt: "Latore banner 2",
 
   ctaText: t[4],
@@ -141,7 +141,7 @@ export default function Hero() {
       {
   type: "image",
   src: "/hoom/hero3.PNG",
-  mobileSrc: "/hoom/heromobile3.PNG",
+  mobileSrc: "/hoom/3.PNG",
   alt: "Latore banner 3",
 
   ctaText: t[4],
